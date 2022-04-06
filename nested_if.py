ff806bd574974ee83342048fa30a47c019957a46
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-04-06 18:50:36 +0000

    unfinished

---
+++ nested_if.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on March 2022
+# This program calculates the circumference of a circle
+#     with inputted radius
+
+
+import constants
+
+
+def main():
+    # I calculate circumference
+    int_year = 0
+
+    # input
+    string_year = input("Enter year: ")
+
+    # process & output
+    try:
+        int_year = int(string_year)
+        if int_year%4 == 0:
+            if int_year%100 == 0:
+                if int_year%400 == 0:
+                    print("Is a leap year")
+                else:
+                    print("Not a leap year")
+            else:
+                print("Is a leap year")
+        else:
+            print("Not a leap year")
+    except Exception:
+        print("Invalid Input")
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
